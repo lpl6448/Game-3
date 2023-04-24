00dc415bd8d7313816991dc55f520d358b5111ec
Nick Altier <nca6109@rit.edu>
2023-04-24 16:47:05 -0400

    Created classes for characters and items in the hub

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : InteractableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Pulls up dialogue box for specific character
+    /// </summary>
+    public override void Clicked()
+    {
+
+    }
+}
