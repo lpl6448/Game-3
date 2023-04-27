b1e4b46d5b7895687f88ebcb0e15f9b05dac065d
nca6109 <nca6109@rit.edu>
2023-04-27 11:07:41 -0400

    Title should look normal again

--- Assets/Scripts/Save+Load/SerializableDictionary.cs
+++ Assets/Scripts/Save+Load/SerializableDictionary.cs
@@ -28,9 +28,9 @@ public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IS
 
         if (keys.Count != values.Count)
         {
-            //Debug.LogError("Tried to deserialize a SerializableDictionary, but the amount of keys ("
-            //    + keys.Count + ") does not match the number of values (" + values.Count
-            //    + ") which indicates that something went wrong");
+            Debug.LogError("Tried to deserialize a SerializableDictionary, but the amount of keys ("
+                + keys.Count + ") does not match the number of values (" + values.Count
+                + ") which indicates that something went wrong");
         }
 
         for (int i = 0; i < keys.Count; i++)
